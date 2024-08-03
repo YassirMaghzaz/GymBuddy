fd59f2f3746ed9ee655d6593374207f3fa4d629c
Yassir Maghzaz <127986901+YassirMaghzaz@users.noreply.github.com>
2024-08-03 16:00:16 +0100

    redirect to notice page after saving the preferences

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -9,7 +9,7 @@ class PreferencesController < ApplicationController
   def create
     @preference = current_user.build_preference(preference_params)
     if @preference.save
-      redirect_to "some_final_path", notice: 'Preferences created successfully.'
+      redirect_to "notice_path", notice: 'Preferences created successfully.'
     else
       render :new
     end
