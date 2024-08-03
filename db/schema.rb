e1c1059a9fa56f6d82df35652f8a04127841917b
Yassir Maghzaz <127986901+YassirMaghzaz@users.noreply.github.com>
2024-08-03 14:44:43 +0100

    change Profile:location column type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_01_191340) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_03_134112) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -101,7 +101,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_01_191340) do
     t.string "last_name"
     t.integer "age"
     t.string "gender"
-    t.text "location"
+    t.string "location"
     t.text "bio"
     t.text "availability"
     t.string "level"
