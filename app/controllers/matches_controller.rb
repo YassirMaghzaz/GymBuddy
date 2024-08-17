cc5d36f4ff99fbb5e8afad9e9b2f821ac2990cf2
Yassir Maghzaz <127986901+YassirMaghzaz@users.noreply.github.com>
2024-08-17 10:18:10 +0100

    define the matches controller

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,50 +1,56 @@
 class MatchesController < ApplicationController
-  before_action :set_match, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  before_action :set_match, only: [:accept, :reject, :destroy]
 
   def index
-    @matches = Match.all
+    matched_profiles_ids = Match.where(profile_id: current_user.profile.id)
+                                .or(Match.where(matched_profile_id: current_user.profile.id))
+                                .pluck(:matched_profile_id, :profile_id).flatten.uniq
+
+    @profile = Profile.where.not(id: matched_profiles_ids + [current_user.profile.id])
+                      .order("RANDOM()")
+                      .first
+
   end
 
-  def show
-  end
-
-  def new
-    @match = Match.new
-  end
 
   def create
-    @match = Match.new(match_params)
+    @match = Match.new(profile_id: current_user.profile.id, matched_profile_id: params[:profile_id], status: "pending")
     if @match.save
-      redirect_to @match, notice: 'Match was successfully created.'
+      redirect_to profile_matches_path, notice: "Match request sent!"
     else
-      render :new
+      redirect_to profile_matches_path, alert: "Something went wrong."
     end
   end
 
-  def edit
+  def pending
+    @pending_matches = Match.where(matched_profile_id: current_user.profile.id, status: "pending")
+
+    @profiles = Profile.where(id: @pending_matches.pluck(:profile_id))
   end
 
-  def update
-    if @match.update(match_params)
-      redirect_to @match, notice: 'Match was successfully updated.'
-    else
-      render :edit
-    end
+
+  def accept
+    @match.update(status: "accepted")
+    redirect_to pending_profile_matches_path, notice: "You have accepted the match!"
+  end
+
+  def reject
+    @match.update(status: "rejected")
+    redirect_to pending_profile_matches_path, notice: "You have rejected the match."
   end
 
   def destroy
-    @match.destroy
-    redirect_to matches_url, notice: 'Match was successfully destroyed.'
+    @match.update(status: "rejected")
+    redirect_to accepted_profile_matches_path, notice: "Match removed."
+  end
+
+  def accepted
+    @accepted_matches = Match.where(profile_id: current_user.profile.id, status: "accepted")
   end
 
   private
 
   def set_match
     @match = Match.find(params[:id])
-  end
-
-  def match_params
-    params.require(:match).permit(:status, :profile_id, :matched_profile_id)
   end
 end
